8884711764fe413a95e89c33b3d8f8d364b15960
Yamir Ramirez <ysramirez@gmail.com>
2016-07-21 11:22:13 -0500

    mysql outsourcing

---
+++ 05_mysql_outsourcing/localfiles/index.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "172.17.0.2";
+$username = "root";
+$password = "1234";
+
+// Create connection
+$conn = new \mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
