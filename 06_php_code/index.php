a6e10a6d0137388c7c860f3165c296d797d224a1
Yamir Ramirez <ysramirez@gmail.com>
2016-08-04 11:32:06 -0500

    update code

--- 06_php_code/index.php
+++ 06_php_code/index.php
@@ -1,2 +1,22 @@
 <?php
-echo "hello";
+
+$servername = "172.18.0.2";
+$username = "root";
+$password = "1234";
+
+// Create connection
+$conn = new \mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
